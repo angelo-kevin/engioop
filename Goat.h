b62b64f9a856cc54603ca06213aa9b1c06536e10
Nixon Andhika <43450143+hidx1@users.noreply.github.com>
2019-03-16 20:45:50 +0700

    Add files via upload

--- Goat.h
+++ Goat.h
@@ -1,3 +1,8 @@
 #include "MilkProducing.h"
+#include "MeatProducing.h"
 
-class Goat : public MilkProducing{};
+class Goat : public MilkProducing, MeatProducing{
+public:
+      string Produce(); //menghasilkan 'susu' dan 'daging'
+      void Sound(); //suara 'petok'
+};
--- Pig.h
+++ Pig.h
@@ -1,3 +1,7 @@
 #include "MeatProducing.h"
 
-class Pig : public MeatProducing{};
+class Pig : public MeatProducing{
+public:
+      string Produce(); //menghasilkan 'daging'
+      void Sound(); //suara 'petok'
+};
