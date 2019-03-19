dfa9db2ec5e23670f3b336417f119e64ffbf0219
Stanley <30459659+Kumaken@users.noreply.github.com>
2019-03-19 23:32:52 +0700

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -6,8 +6,6 @@ class Cell{
   public:
     // Base Class Constructor
     Cell();
-    // Virtual Destructor
-    virtual ~Cells(){};
     // Virtual Method Unique symbol for every types of objects in cell
     char showSymbol();
 };
