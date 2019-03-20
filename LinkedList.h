2424bf5d3f22ff0b8e289dd6e2947a261ebc9c24
jesslyn1999 <36306615+jesslyn1999@users.noreply.github.com>
2019-03-20 13:01:18 +0700

    Add files via upload

--- LinkedList.h
+++ LinkedList.h
@@ -1,12 +1,24 @@
-
 template<class T>
 class Node{
     private:
         Node<T>* next;
         T data;
-        int idx;
+        int idx; //start from 0 to length-1
     public:
         friend class LinkedList;
+        Node(T _data, int _idx){
+            next = NULL;
+            data = _data;
+            idx =  _idx;
+        }
+        Node(Node& node){
+            next = new Node(node.next);
+            data = node.data;
+            idx = node.idx;
+        }
+        ~Node(){
+            delete next;
+        }
 };
 
 template<class T>
@@ -15,13 +27,79 @@ class LinkedList{
         Node<T>* head;
         int length;
     public: 
-        LinkedList(); //constructor
-        LinkedList(LinkedList&)
-        ~LinkedList(); //destructor
-        int find(T); //mengembalikan idx ditemukan data
-        bool isEmpty(); //mengembalikan true jika head=NULL atau sebaliknya
-        void add(T); //menambah elemen di index paling belakang dari LinkedList
-        void remove(T); //mengahapus elemen T tertentu dari LinkedList
-        T get(int); //mengembalikan data dari LinkedList di idx tertentu
+        LinkedList() //constructor
+        {
+            head = NULL;
+            length = 0;
+        }
+        LinkedList(LinkedList& L){
+            head = new Node(L.head);
+            length = L.length;
+        }
+        ~LinkedList() //destructor
+        {
+            delete head;
+        }
+        int find(T _data) //mengembalikan idx ditemukan data
+        {
+            Node* ptr = head;
+            while(ptr->idx < length && ptr->data!=_data) {
+                ptr=ptr->next;
+            }
+            if (ptr->data == _data) return ptr->idx
+            else return -1; //not found;
+        }
+        bool isEmpty() //mengembalikan true jika head=NULL atau sebaliknya{
+            return (head==NULL);
+        }
+        void add(T _data) //menambah elemen di index paling belakang dari LinkedList
+        {
+            if (head == NULL){
+                Node node(_data,0);
+                head = new Node(node);
+            }else{
+                Node* ptr = head;
+                while (ptr->next!=NULL){
+                    ptr=ptr->next;
+                }
+                Node node(_data, ptr->idx+1)
+                ptr->next = new Node(node);
+            }
+            length++;
+
+        }
+        void remove(T _data) //mengahapus elemen T pertama dari LinkedList
+        {
+            if(head!=NULL){
+                Node* ptr = head;
+                while(ptr->next->idx < length && ptr->next->data!=_data) {
+                    ptr=ptr->next;
+                }
+                if (ptr->next->data == _data){
+                    Node* ptrdel = ptr->next;
+                   
+                    if (ptr->next->next == NULL){ //at the last of elements
+                        ptr->next=NULL;
+                    }else{
+                        ptr->next = ptr->next->next;
+                        ptr = ptr->next;
+                        while(ptr!=NULL){
+                            ptr->id-=1;
+                            ptr=ptr->next;
+                        }
+                    }
+                    delete ptrdel;
+                }
+                
+            }
+        }
+        T get(int _idx); //assume idx is in range //mengembalikan data dari LinkedList di idx tertentu
+        {
+            Node* ptr=head;
+            while(ptr->idx < length && ptr->idx!=_idx) {
+                ptr=ptr->next;
+            }
+            if (ptr->idx == _idx) return ptr->data 
+        }
 
 };
