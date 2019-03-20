f863292c2cfb70233f983d624bdcd703b38c6665
jesslyn1999 <36306615+jesslyn1999@users.noreply.github.com>
2019-03-20 13:17:24 +0700

    Update Barn.h

--- Barn.h
+++ Barn.h
@@ -1,6 +1,7 @@
 #include "Land.h"
-
+/* Kelas Barn adalah turunan dari kelas Land dan merepresentasikan lahan pijakan yang di dalam kandang dan MeatProducing Animals.*/
 class Barn : public Land{
     public:
+        //Konstruktor
         Barn();
 };
