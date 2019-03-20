567c3abb66fdd2c31c03ae5849076248e6efb9f9
jesslyn1999 <36306615+jesslyn1999@users.noreply.github.com>
2019-03-20 09:52:08 +0700

    Add files via upload

--- LinkedList.h
+++ LinkedList.h
@@ -1,3 +1,4 @@
+
 template<class T>
 class Node{
     private:
@@ -9,17 +10,18 @@ class Node{
 };
 
 template<class T>
-class LinkedList: public Inventory{
+class LinkedList{
     private:
         Node<T>* head;
         int length;
     public: 
-        LinkedList(); 
-        ~LinkedList();
-        int find(T);
-        bool isEmpty();
-        void add(T);
-        void remove(T);
-        T get(int);
+        LinkedList(); //constructor
+        LinkedList(LinkedList&)
+        ~LinkedList(); //destructor
+        int find(T); //mengembalikan idx ditemukan data
+        bool isEmpty(); //mengembalikan true jika head=NULL atau sebaliknya
+        void add(T); //menambah elemen di index paling belakang dari LinkedList
+        void remove(T); //mengahapus elemen T tertentu dari LinkedList
+        T get(int); //mengembalikan data dari LinkedList di idx tertentu
 
 };
--- Player.h
+++ Player.h
@@ -1,29 +1,26 @@
 #include<iostream>
 #include<vector>
 #include<string>
-#include"Product.h"
-#include "Landtypes.h"
-#include "cells.h"
+#include "Product.h"
+#include"LinkedList.h"
+#include "Cell.h"
 
-class Player{
+class Player : Cell{
     private:
-        private:
-        int row;
-        int col;
-        vector<Product> backpack;
+        int row; //posisi x player
+        int col; //posisi y player. 
+        LinkedList<Product> backpack; //Menampung linkedlist of Product
         //int nell; //backpack
-        int pouch; // water
+        int pouch; // Kantong air
     public:
-        Player();
-        ~Player();
-        void Talk(Cells*);
-        void Interact(Cells*);
-        void Kill(Cells*);
-        void Grow(Cells*);
-        void Mix(string);
-        void classIdentifier(char c, vector<Cells*> &v);
-        void setPosition(char);
-        Cells* getPosition(char); //ask for position in 4 directions
-        void loadMap();
+        Player(); //constructor
+        //~Player(); //tidak perlu 
+        void Talk(Cells*); //memanggil method sound dari animal
+        void Interact(Cells*); //memanggil method sesuai dengan apa yang ada di sekitar player
+        void Kill(Cells*); //destructor meatproducing animal
+        void Grow(Cells*); //memanggil method growGrass
+        void Mix(string); //menerima parameter string yaitu side product apa yang mau dibuat. Lalu, melihat apakah backpack mempunyai bahan yang lengkap
+        void setPosition(char); //set the position of player
+        Cells* getPosition(char); //ask for cells in 4 directions surround player
 
 };
