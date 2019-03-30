25293e1039275ac218e35dd47cce984fccf9e4e2
Stanley <30459659+Kumaken@users.noreply.github.com>
2019-03-30 21:29:58 +0700

    LAPOR: Jesnat mau kerjain OS

---
+++ Cell.cpp
@@ -0,0 +1,13 @@
+// Base Class : Cell
+/* Kelas Cell adalah base class yang merepresentasikan setiap objek-objek yang akan muncul di map, seperti semua objek Facility, objek Land, objek Farm Animal, dan Player. Kelas ini memiliki atribut symbol bertipe char yang unik bagi setiap objek di map dan method getter untuk simbol tersebut.*/
+#include "Cell.h"
+
+// Base Class Constructor
+Cell::Cell(){
+    //Do Nothing LOL
+}
+
+// Method to show Unique symbol for every types of objects in cell
+char Cell::showSymbol(){
+    return this->Symbol;
+}
--- Cell.h
+++ Cell.h
@@ -1,12 +1,15 @@
+#ifndef CELL_H
+#define CELL_H
 // Base Class : Cell
 /* Kelas Cell adalah base class yang merepresentasikan setiap objek-objek yang akan muncul di map, seperti semua objek Facility, objek Land, objek Farm Animal, dan Player. Kelas ini memiliki atribut symbol bertipe char yang unik bagi setiap objek di map dan method getter untuk simbol tersebut.*/
 class Cell{
-  private:
+  protected:
     // Unique symbol for each objects
     char Symbol;
   public:
     // Base Class Constructor
     Cell();
-    // Virtual Method Unique symbol for every types of objects in cell
+    // Method to show Unique symbol for every types of objects in cell
     char showSymbol();
 };
+#endif
