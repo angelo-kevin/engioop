59aa4d5fd7ad3435be3de1ee8555477fe9b9281e
Kumaken <abelstanley@ymail.com>
2019-04-04 20:51:39 +0700

    I MADE GOOGLE TEST!!!

--- common.h
+++ common.h
@@ -1,8 +1,10 @@
-#ifndef HEADERS_H_DECLARED
-#define HEADERS_H_DECLARED
+#ifndef COMMON_H
+#define COMMON_H
 #include "Cell.h"
 #include <vector>
 using namespace std;
 
+//Global matrix variable for map accross files:
 extern vector<vector<Cell*> > gamemap;
+//After this you need to define it again at .cpp file ONCE
 #endif
